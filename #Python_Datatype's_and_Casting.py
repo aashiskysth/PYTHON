efd5f3c8dc07a2f927241a2124fae4305ba071a3
Aashish Kumar Karna <aashishkumarkarna2024@gmail.com>
2025-06-15 21:21:51 +0545

    Create #Python_Datatype's_and_Casting.py

---
+++ #Python_Datatype's_and_Casting.py
@@ -0,0 +1,17 @@
+>>> #Python Datatype's and Casting
+>>> y=6
+>>> print (type (6) )
+<class 'int'>
+>>> print (10 > 9)
+True
+>>> print (10 == 9)
+False
+>>> print (10<9)
+False
+>>>first = 100
+>>> second = 200000
+>>> c = first + second
+>>> print(c)
+200100
+
+
