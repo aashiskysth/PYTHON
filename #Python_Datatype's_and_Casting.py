f659288314e0d7c58231ba04a84e06d6a03c975e
Aashish Kumar Karna <aashishkumarkarna2024@gmail.com>
2025-06-15 21:49:27 +0545

    Update #Python_Datatype's_and_Casting.py

--- #Python_Datatype's_and_Casting.py
+++ #Python_Datatype's_and_Casting.py
@@ -13,5 +13,53 @@ False
 >>> c = first + second
 >>> print(c)
 200100
+""" 
+To specify a type on to a variable, 
+casting can be done. Python is an object-oriented language, 
+and uses classes to define data types, 
+including its primary types. so,
+casting in python is done using constructor functions:
+a) int () - To convert the float and string variable to the integer variable.
+b) float () - To convert the integer and string variable to decimal value.
+c) str() - To convert the integer and float variable to string type variable.
+"""
+>>> x = int (1)
+>>> y =int (2.4) # round off to convert into integer
+>>> z =int ("3")
+>>> print(x)
+1
+>>> print(y)
+2
+>>> print(z)
+3
+
+
+>>> x = str ("U1")
+>>> y = str (9)
+>>> z = str (8.4)
+>>> print(x)
+U1
+>>> print(y)
+9
+>>> print(z)
+8.4
+
+
+>>> x=float(9)
+>>> y=float(2.9)
+>>> z=float("8")
+>>> print(x)
+9.0
+>>> print(y)
+2.9
+>>> print(z)
+8.0
+>>>
+
+
+
+
+
+
 
 
